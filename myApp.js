d101b69b4b8082714a9a59227a98cafd1105e95f
cEDUARDOrv <148309799+RochaVz@users.noreply.github.com>
2024-04-01 07:24:05 -0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,35 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config();
+//#1
+// console.log("Hello World")
 
-app.get("/", (request, response) => {
-  response.send("Hello Express");
+//2# app.get("/", (req, res) => {
+//   res.send("Hello Express")
+//})
+
+//4
+
+app.use("/public", express.static(__dirname + "/public"));
+
+//3#
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+//#5
+
+// app.get("/json", (req, res) => {
+//   res.json({ message: "Hello json" });
+// });
+
+//#6
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
 });
 
 module.exports = app;
