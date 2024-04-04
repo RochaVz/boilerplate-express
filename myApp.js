950ae71ea66018af34ad7b0786a355ce5081d835
cEDUARDOrv <148309799+RochaVz@users.noreply.github.com>
2024-04-04 05:31:44 -0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,16 +1,27 @@
 let express = require("express");
 let app = express();
 require("dotenv").config();
+
 //#1
 // console.log("Hello World")
-
-//2# app.get("/", (req, res) => {
-//   res.send("Hello Express")
-//})
 
 //4
 
 app.use("/public", express.static(__dirname + "/public"));
+
+//#7
+app.use((req, res, next) => {
+
+     let string = `${req.method} ${req.path} - ${req.ip}`
+ console.log(string) 
+
+    next();
+})
+
+//2#
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
 //3#
 app.get("/", (req, res) => {
@@ -33,6 +44,7 @@ app.get("/json", (req, res) => {
 });
 
 module.exports = app;
+
 
 
 
